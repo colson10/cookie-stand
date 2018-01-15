7ae6686c1b440b94e4076491dbb3f46812f4a39f
Carl Olson <colson10@gmail.com>
2018-01-15 14:16:38 -0800

    location hours array and pseudo code added to app.js, changed title for sales.html

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 'use strict';
 
+var locationHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// Create object literal for each location
+// Create properties for min, max, average
+// I also need a method for determining a random number of customers per hour
+// Calculate and store the projected number of cookies purchased for each hour by multiplying the random number for that location times the average number of cookies per customer
+// Store the results for each location in a separate array... perhaps as a property of the object representing that location
+// Display the values of each array as unordered lists in the browser
+// Calculate and display the sum of these hourly totals
