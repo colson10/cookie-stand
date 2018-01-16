b51309c82a3a8cd548641395f9ae0ecf51cac6ed
Carl Olson <colson10@gmail.com>
2018-01-15 16:55:41 -0800

    second object complete, updates to html and css to make rendered data easier to read

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ var firstAndPike = {
   },
   render: function() {
     // access ul from index.html
-    var ulEL = document.getElementById('pike');
+    var ulEl = document.getElementById('pike');
 
     for (var i = 0; i < this.hourlySalesProjections.length; i++) {
       // create list item elements
-      var liEL = document.createElement('li');
+      var liEl = document.createElement('li');
       // give items content
-      liEL.textContent = locationHours[i] + ': ' + this.hourlySalesProjections[i];
+      liEl.textContent = locationHours[i] + ': ' + this.hourlySalesProjections[i];
       // append list items to ul element
-      ulEL.appendChild(liEL);
+      ulEl.appendChild(liEl);
     }
   },
   renderTotalDailySales: function() {
@@ -47,12 +47,64 @@ var firstAndPike = {
     for (var i = 0; i < this.hourlySalesProjections.length; i++) {
       total += this.hourlySalesProjections[i];
     }
-    var ulEL = document.getElementById('pike');
-    var liEL = document.createElement('li');
-    liEL.textContent = 'Total: ' + total + ' cookies sold per day.';
-    ulEL.appendChild(liEL);
+    var ulEl = document.getElementById('pike');
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + total + ' cookies sold per day.';
+    ulEl.appendChild(liEl);
   },
 };
 firstAndPike.populateHourlySales();
 firstAndPike.render();
 firstAndPike.renderTotalDailySales();
+
+// Create object literal for each location
+// Create properties for min, max, average
+// I also need a method for determining a random number of customers per hour
+// Calculate and store the projected number of cookies purchased for each hour by multiplying the random number for that location times the average number of cookies per customer
+// Store the results for each location in a separate array... perhaps as a property of the object representing that location
+// Display the values of each array as unordered lists in the browser
+// Calculate and display the sum of these hourly totals
+
+var seatac = {
+  minCust: 3,
+  maxCust: 24,
+  aveCust: 1.2,
+  hourlySalesProjections: [],
+  randomNum: function () {
+    return Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  aveCookiesPerHour: function() {
+    return Math.round(this.randomNum() * this.aveCust);
+  },
+  populateHourlySales: function() {
+    for (var i = 0; i < locationHours.length; i++) {
+      this.hourlySalesProjections.push(this.aveCookiesPerHour());
+    }
+  },
+  render: function() {
+    var ulEl = document.getElementById('seatac');
+    
+    for (var i = 0; i < this.hourlySalesProjections.length; i++) {
+      //create list item elements
+      //give items content
+      //append to ul element
+      var liEl = document.createElement('li');
+      liEl.textContent = locationHours[i] + ': ' + this.hourlySalesProjections[i];
+      ulEl.appendChild(liEl);
+    }
+  },
+  renderTotalDailySales: function() {
+    var total = 0;
+    for (var i = 0; i < this.hourlySalesProjections.length; i++) {
+      total += this.hourlySalesProjections[i];
+    }
+    var ulEl = document.getElementById('seatac');
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + total + ' cookies sold per day.';
+    ulEl.appendChild(liEl);
+  }
+};
+
+seatac.populateHourlySales();
+seatac.render();
+seatac.renderTotalDailySales();
