003b4edb37555239987ebf1a00ddb9f81225232c
Carl Olson <colson10@gmail.com>
2018-01-15 20:55:57 -0800

    fifth object added, moved function calls to bottom

--- app.js
+++ app.js
@@ -2,14 +2,6 @@
 
 var locationHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-// Create object literal for each location
-// Create properties for min, max, average
-// I also need a method for determining a random number of customers per hour
-// Calculate and store the projected number of cookies purchased for each hour by multiplying the random number for that location times the average number of cookies per customer
-// Store the results for each location in a separate array... perhaps as a property of the object representing that location
-// Display the values of each array as unordered lists in the browser
-// Calculate and display the sum of these hourly totals
-
 var firstAndPike = {
   minCust: 23,
   maxCust: 65,
@@ -22,48 +14,32 @@ var firstAndPike = {
     return Math.round(this.randomNum() * this.aveCust);
   },
   populateHourlySales: function() {
-    // I want to return an array with aveCookiesPerHour for each index in the array locationHours (15)
-    // for loop running through the length of locationHours, push each value to an empty array.
-    // return the array
     for (var i = 0; i < locationHours.length; i++) {
       this.hourlySalesProjections.push(this.aveCookiesPerHour());
     }
   },
   render: function() {
-    // access ul from index.html
     var ulEl = document.getElementById('pike');
 
     for (var i = 0; i < this.hourlySalesProjections.length; i++) {
-      // create list item elements
       var liEl = document.createElement('li');
-      // give items content
       liEl.textContent = locationHours[i] + ': ' + this.hourlySalesProjections[i];
-      // append list items to ul element
       ulEl.appendChild(liEl);
     }
   },
   renderTotalDailySales: function() {
     var total = 0;
+
     for (var i = 0; i < this.hourlySalesProjections.length; i++) {
       total += this.hourlySalesProjections[i];
     }
+
     var ulEl = document.getElementById('pike');
     var liEl = document.createElement('li');
     liEl.textContent = 'Total: ' + total + ' cookies sold per day.';
     ulEl.appendChild(liEl);
   },
 };
-firstAndPike.populateHourlySales();
-firstAndPike.render();
-firstAndPike.renderTotalDailySales();
-
-// Create object literal for each location
-// Create properties for min, max, average
-// I also need a method for determining a random number of customers per hour
-// Calculate and store the projected number of cookies purchased for each hour by multiplying the random number for that location times the average number of cookies per customer
-// Store the results for each location in a separate array... perhaps as a property of the object representing that location
-// Display the values of each array as unordered lists in the browser
-// Calculate and display the sum of these hourly totals
 
 var seatac = {
   minCust: 3,
@@ -85,9 +61,6 @@ var seatac = {
     var ulEl = document.getElementById('seatac');
     
     for (var i = 0; i < this.hourlySalesProjections.length; i++) {
-      //create list item elements
-      //give items content
-      //append to ul element
       var liEl = document.createElement('li');
       liEl.textContent = locationHours[i] + ': ' + this.hourlySalesProjections[i];
       ulEl.appendChild(liEl);
@@ -95,19 +68,17 @@ var seatac = {
   },
   renderTotalDailySales: function() {
     var total = 0;
+
     for (var i = 0; i < this.hourlySalesProjections.length; i++) {
       total += this.hourlySalesProjections[i];
     }
+
     var ulEl = document.getElementById('seatac');
     var liEl = document.createElement('li');
     liEl.textContent = 'Total: ' + total + ' cookies sold per day.';
     ulEl.appendChild(liEl);
   }
 };
-
-seatac.populateHourlySales();
-seatac.render();
-seatac.renderTotalDailySales();
 
 var seattleCenter = {
   minCust: 11,
@@ -148,11 +119,6 @@ var seattleCenter = {
   }
 };
 
-seattleCenter.populateHourlySales();
-seattleCenter.render();
-seattleCenter.renderTotalDailySales();
-
-
 var capitol = {
   minCust: 20,
   maxCust: 38,
@@ -185,12 +151,64 @@ var capitol = {
     for (var i = 0; i < this.hourlySalesProjections.length; i++) {
       total += this.hourlySalesProjections[i];
     }
+
     var liEl = document.createElement('li');
     liEl.textContent = 'Total: ' + total + ' cookies sold per day.';
     ulEl.appendChild(liEl);
   }
 };
 
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  aveCust: 4.6,
+  hourlySalesProjections: [],
+  randomNum: function() {
+    return Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  aveCookiesPerHour: function() {
+    return Math.round(this.randomNum() * this.aveCust);
+  },
+  populateHourlySales: function() {
+    for (var i = 0; i < locationHours.length; i++) {
+      this.hourlySalesProjections.push(this.aveCookiesPerHour());
+    }
+  },
+  render: function() {
+    var ulEl = document.getElementById('alki');
+
+    for (var i = 0; i < this.hourlySalesProjections.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = locationHours[i] + ': ' + this.hourlySalesProjections[i];
+      ulEl.appendChild(liEl);
+    }
+  },
+  renderTotalDailySales: function() {
+    var ulEl = document.getElementById('alki');
+    var total = 0;
+
+    for (var i = 0; i < this.hourlySalesProjections.length; i++) {
+      total += this.hourlySalesProjections[i];
+    }
+
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + total + ' cookies sold per day.';
+    ulEl.appendChild(liEl);
+  }
+};
+
+firstAndPike.populateHourlySales();
+firstAndPike.render();
+firstAndPike.renderTotalDailySales();
+seatac.populateHourlySales();
+seatac.render();
+seatac.renderTotalDailySales();
+seattleCenter.populateHourlySales();
+seattleCenter.render();
+seattleCenter.renderTotalDailySales();
 capitol.populateHourlySales();
 capitol.render();
 capitol.renderTotalDailySales();
+alki.populateHourlySales();
+alki.render();
+alki.renderTotalDailySales();
