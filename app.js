94ede865aafc580ac44687c4a0a23b47ebb175c6
Carl Olson <colson10@gmail.com>
2018-01-16 15:04:00 -0800

    added totals without total of totals

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 'use strict';
 
 var locationHours = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
-
-// CONSTRUCTOR FUNCTION
-// Make variable to get the table element by id.
 var storeTable = document.getElementById('stores');
-// make a constructor that will then make our stores
 
 function Store(name, minCust, maxCust, aveCust) {
   this.name = name;
@@ -15,17 +11,10 @@ function Store(name, minCust, maxCust, aveCust) {
   this.hourlySalesProjections = [];
 }
 
-// put render methods outside the constructor
-
-// Store.prototype.aveCookiesPerHour = function() {
-//   return Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)) * this.aveCust);
-// };
-
 Store.prototype.populateHourlySales = function() {
   for (var i = 0; i < locationHours.length - 2; i++) {
     this.hourlySalesProjections.push(Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)) * this.aveCust));
   }
-
 };
 
 Store.prototype.render = function() {
@@ -47,7 +36,6 @@ Store.prototype.render = function() {
   storeTable.appendChild(trEl);
 };
 
-// separate function to make table header
 function makeHeaderRow() {
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
@@ -58,14 +46,28 @@ function makeHeaderRow() {
   }
   storeTable.appendChild(trEl);
 }
+// last row totaling all locations for each hour and total
+function rowOfTotals() {
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = 'Location Totals';
+  tdEl.setAttribute('id', 'name');
+  trEl.appendChild(tdEl);
+  for (var i = 0; i < firstAndPike.hourlySalesProjections.length; i++) {
+    var total = firstAndPike.hourlySalesProjections[i] + seaTac.hourlySalesProjections[i] + seattleCenter.hourlySalesProjections[i] + capitolHill.hourlySalesProjections[i] + alki.hourlySalesProjections[i];
+    tdEl = document.createElement('td');
+    tdEl.textContent = total;
+    trEl.appendChild(tdEl);
+  }
+  storeTable.appendChild(trEl);
+}
 
-// create store instances
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
 var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
-// call functions
+
 makeHeaderRow();
 firstAndPike.populateHourlySales();
 firstAndPike.render();
@@ -77,6 +79,8 @@ capitolHill.populateHourlySales();
 capitolHill.render();
 alki.populateHourlySales();
 alki.render();
+rowOfTotals();
+
 
 
 
