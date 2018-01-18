bf08bc61921890474c5ecd0a37639615bfea85f1
Carl Olson <colson10@gmail.com>
2018-01-17 16:05:34 -0800

    completed form and functioning properly

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var locationHours = ['Location', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+var locationHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var storeTable = document.getElementById('stores');
 var storeForm = document.getElementById('store-form');
 function reducer(a, b) {
@@ -19,8 +19,10 @@ function Store(name, minCust, maxCust, aveCust) {
 }
 
 Store.prototype.populateHourlySales = function() {
-  for (var i = 0; i < locationHours.length - 2; i++) {
-    this.hourlySalesProjections.push(Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)) * this.aveCust));
+  if (this.hourlySalesProjections.length === 0) {
+    for (var i = 0; i < locationHours.length; i++) {
+      this.hourlySalesProjections.push(Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)) * this.aveCust));
+    }
   }
   return this;
 };
@@ -32,7 +34,7 @@ Store.prototype.render = function() {
   tdEl.setAttribute('class', 'name');
   trEl.appendChild(tdEl);
   var total = 0;
-  for (var i = 0; i < this.hourlySalesProjections.length; i++) {
+  for (var i = 0; i < locationHours.length; i++) {
     tdEl = document.createElement('td');
     tdEl.textContent = this.hourlySalesProjections[i];
     trEl.appendChild(tdEl);
@@ -47,12 +49,19 @@ Store.prototype.render = function() {
 function makeHeaderRow() {
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
+  thEl.textContent = 'Location';
+  thEl.setAttribute('class', 'header-row');
+  trEl.appendChild(thEl);
   for (var i = 0; i < locationHours.length; i++) {
     thEl = document.createElement('th');
     thEl.textContent = locationHours[i];
     thEl.setAttribute('class', 'header-row');
     trEl.appendChild(thEl);
   }
+  thEl = document.createElement('th');
+  thEl.textContent = 'Total';
+  thEl.setAttribute('class', 'header-row');
+  trEl.appendChild(thEl);
   storeTable.appendChild(trEl);
 }
 
@@ -62,7 +71,7 @@ function rowOfTotals() {
   tdEl.textContent = 'Totals';
   tdEl.setAttribute('id', 'total');
   trEl.appendChild(tdEl);
-  for (var i = 0; i < firstAndPike.hourlySalesProjections.length; i++) {
+  for (var i = 0; i < locationHours.length; i++) {
     var total = 0;
     for (var x = 0; x < allLocations.length; x++) {
       total += allLocations[x].hourlySalesProjections[i];
@@ -91,9 +100,17 @@ function addNewStore(event) {
   event.preventDefault();
 
   var newName = event.target.storeName.value;
-  var newMinCust = event.target.minCustPer.value;
-  var newMaxCust = event.target.maxCustPer.value;
-  var newAveCust = event.target.aveCookiePer.value;
+  console.log(newName);
+  console.log(typeof newName);
+  var newMinCust = event.target.minCustPer.valueAsNumber;
+  console.log(newMinCust);
+  console.log(typeof newMinCust);
+  var newMaxCust = event.target.maxCustPer.valueAsNumber;
+  console.log(newMaxCust);
+  console.log(typeof newMaxCust);
+  var newAveCust = event.target.aveCookiePer.valueAsNumber;
+  console.log(newAveCust);
+  console.log(typeof newAveCust);
 
   new Store(newName, newMinCust, newMaxCust, newAveCust);
 
@@ -103,7 +120,7 @@ function addNewStore(event) {
   rowOfTotals();
 }
 
-var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
+new Store('1st and Pike', 23, 65, 6.3);
 new Store('SeaTac Airport', 3, 24, 1.2);
 new Store('Seattle Center', 11, 38, 3.7);
 new Store('Capitol Hill', 20, 38, 2.3);
